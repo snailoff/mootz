66e736e6053a61b3ae2e82584cef97a4606e398a
snailoff <snailoff@lastRuby.local>
2019-08-23 15:36:06 +0900

    watcher 추가

--- mootz.clj
+++ mootz.clj
@@ -1,5 +1,15 @@
-(ns mootz)
+(ns mootz
+  (:require [clojure.edn]
+            [hawk.core :as hawk]))
+
+(def config (clojure.edn/read-string (slurp "config.edn")))
 
 (defn -main
   [& args]
-  (println "hi mootz."))
+  (println "hi mootz.")
+  (println (:rootpath config))
+  (hawk/watch! [{:paths [(:rootpath config)]
+                 :handler (fn [ctx e]
+                            (println "event: " (.getAbsolutePath (:file e)) (:kind e))
+                            (println "context: " ctx)
+                            ctx)}]))
--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject rootm "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [hawk "0.2.11"]]
   :source-paths ["."]
   :main mootz)
