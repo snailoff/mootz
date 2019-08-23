d5cd799efdc27b6c1f9dc06ee88e3525adaec8b7
snailoff <snailoff@lastRuby.local>
2019-08-23 16:31:08 +0900

    ring 추가

--- mootz.clj
+++ mootz.clj
@@ -1,15 +1,29 @@
 (ns mootz
+  (:gen-class)
   (:require [clojure.edn]
-            [hawk.core :as hawk]))
+            [hawk.core :as hawk])
+  (:use ring.adapter.jetty))
 
 (def config (clojure.edn/read-string (slurp "config.edn")))
 
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "hi ring."})
+
 (defn -main
   [& args]
+
   (println "hi mootz.")
   (println (:rootpath config))
   (hawk/watch! [{:paths [(:rootpath config)]
                  :handler (fn [ctx e]
-                            (println "event: " (.getAbsolutePath (:file e)) (:kind e))
-                            (println "context: " ctx)
-                            ctx)}]))
+                            (println "event: "
+                                     (.getAbsolutePath (:file e))
+                                     (:kind e))
+                            (println "context: "
+                                     ctx)
+                            ctx)}])
+
+  (run-jetty handler {:port 3000}))
+
--- project.clj
+++ project.clj
@@ -1,5 +1,13 @@
 (defproject rootm "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [hawk "0.2.11"]]
+                 [hawk "0.2.11"]
+                 [ring "1.7.1"]
+                 [ring/ring-jetty-adapter "1.6.3"]]
+
+
   :source-paths ["."]
-  :main mootz)
+  :main mootz
+  :aot [mootz]
+  :clean-targets ^{:protect false} ["target"]
+  )
+
