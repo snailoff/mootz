edea31677e033f6e1ed7caa25f789729be692ae5
snailoff <snailoff@lastRuby.local>
2019-08-25 01:28:42 +0900

    special file replace

--- mootz.clj
+++ mootz.clj
@@ -1,16 +1,36 @@
 (ns mootz
   (:gen-class)
   (:require [clojure.edn]
+            [clojure.java.io :as io]
+            [clojure.string :refer [replace]]
             [hawk.core :as hawk])
   (:use ring.adapter.jetty))
 
 (def config (clojure.edn/read-string (slurp "config.edn")))
 
+(def index-file
+  (slurp (str "themes/" (:theme config) "/index.html")))
+
+(defn special-file [name]
+  (let [path (str (:rootpath config) "/" name)]
+    (if (.exists (io/file path))
+      (slurp path)
+      "")))
+
+
+(defn render [request]
+  (let [content index-file]
+    (-> content
+        (replace #"__MAIN_NAME__" (special-file "_main_name"))
+        (replace #"__HEADER__" (special-file "_header"))
+        (replace #"__FOOTER__" (special-file "_footer")))))
+
 (defn handler [request]
   (println (:uri request))
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body (slurp (str "themes/" (:theme config) "/index.html"))})
+      :body (render request)})
+
 
 (defn -main
   [& args]
