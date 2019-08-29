f88017b38f14e03700c5b82d41cab3bb59df1292
snailoff <snailoff@lastRuby.local>
2019-08-30 02:07:07 +0900

    depth, files 링크처리. 레이아웃 테스트

--- mootz/core.clj
+++ mootz/core.clj
@@ -15,7 +15,7 @@
 
 (def config (clojure.edn/read-string (slurp "config.edn")))
 
-(def index-file (slurp (str "resources/themes/" (:theme config) "/index.html")))
+(defn index-file [] (slurp (str "resources/themes/" (:theme config) "/index.html")))
 
 (defn full-path [path]
   (str "resources/public/root/" path))
@@ -35,58 +35,66 @@
       ))
 
 (defn depth [path]
-  (let [cpath (if (= path ".") path (str "./" path))]
-    (str/join "<br />"
-              (map #(str "<a href=\"" %1 "\">"
-                         (str/replace %1 #"^.*/" "")
-                         "</a>")
-                   (loop [p path
-                          result []]
-                     (if (= p "")
-                       (reverse result)
-                       (recur (str/replace p #"/?[^/]*?$" "")
-                              (conj result p))))
-    ))))
+  (let [cpath (if (= path ".")
+                path
+                (str "./" path))]
+        (str/join " . "
+                  (map #(if (= %1 "")
+                          "> <a href=\"/\"><b>root</b></a>"
+                          (str "<a href=\"" %1 "\"><b>"
+                            (str/replace %1 #"^.*/" "")
+                            "</b></a>"))
+                      (loop [p path
+                              result []]
+                        (if (= p "")
+                          (reverse (conj result ""))
+                          (recur (str/replace p #"/?[^/]*?$" "")
+                                  (conj result p))))
+                      ))))
 
 (defn file-list [path]
-  (str (str/join "<br />"
-                 (map #(str "<a href=\"#\">"
-                            (str/replace %1 #"^.*/" "")
-                            "</a>")
+  (str (str/join " . "
+                 (map #(str "<a href=\""
+                            (str/replace %1 #"^.*/root/" "/")
+                            "\"><b>"
+                             (.getName %1)
+                            "</b></a>")
                       (filter #(.isDirectory %)
                               (.listFiles (io/file (full-path path))))))
-       "<br />"
-       (str/join "<br />"
-                 (map #(str "- <a href=\"#\">"
-                            (str/replace %1 #"^.*/" "")
+        "<br />"
+        (str/join " . "
+                  (map #(str " <a href=\""
+                             (str/replace %1 #"^resources/public/root/" "/")
+                             " \">"
+                             (str/replace (.getName %1) #".mz$" "")
                             "</a>")
                       (filter #(.endsWith (.getName %) ".mz")
-                      (.listFiles (io/file (full-path path))))))))
+                              (.listFiles (io/file (full-path path))))))))
 
-(defn parse-directory [path]
+(defn parse-directory [uri]
   {:isdir true
-   :depth (depth path)
-   :list (file-list path)
-   :name (if (= path ".") "/"
-             (str/replace path #".*/" ""))
-   :content (apply-extensions (util/slurp-exists (full-path (str path "/_"))))
-   :date (date-string (full-path path))
+   :depth (depth uri)
+   :list (file-list uri)
+   :name (if (= uri "") "/"
+             (str/replace uri #".*/" ""))
+   :content (apply-extensions (util/slurp-exists (full-path (str uri "/_"))))
+   :date (date-string (full-path uri))
    })
 
-(defn parse-file [path]
+(defn parse-file [uri]
   {:isdir false
-   :depth (depth (str/replace path #"/?[^/]*?.mz$" ""))
-   :list (file-list (str/replace path #"/?[^/]*?.mz$" ""))
-   :name (str/replace path #"^.*/|.mz$" "")
-   :content (apply-extensions (slurp (full-path path)))
-   :date (date-string (full-path path))
+   :depth (depth (str/replace uri #"/?[^/]*?.mz$" ""))
+   :list (file-list (str/replace uri #"/?[^/]*?.mz$" ""))
+   :name (str/replace uri #"^.*/|.mz$" "")
+   :content (apply-extensions (slurp (full-path uri)))
+   :date (date-string (full-path uri))
    })
 
-(defn parse-request [path]
-  (if (.exists (io/file (full-path path)))
-    (if (.isDirectory (io/file (full-path path)))
-      (parse-directory path)
-      (parse-file path))
+(defn parse-request [uri]
+  (if (.exists (io/file (full-path uri)))
+    (if (.isDirectory (io/file (full-path uri)))
+      (parse-directory uri)
+      (parse-file uri))
 
     (parse-directory ".")))
 
@@ -103,12 +111,12 @@
   (if (some? (re-matches #"favicon.ico" (:uri request)))
     ""
 ;    (let [pinfo (parse-request (decode-path (:uri request)))]
-     (let [pinfo (parse-request (str/replace (:uri request) #"^/*|/*$" ""))]
+     (let [pinfo (parse-request (str/replace (:uri request) #"/*$" ""))]
 
       ;왜 안되지.
       ;(assoc pinfo :content (apply-extensions pinfo))
 
-      (-> index-file
+      (-> (index-file)
           (str/replace #"__PAGE_NAME__" (:name pinfo))
           (str/replace #"__PAGE_CONTENT__" (:content pinfo))
           (str/replace #"__PAGE_DATE__" (:date pinfo))
